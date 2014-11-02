21d6ff6d5baecd42738d8f41b622a29bffc8c7ea
MATSUU Takuto <matsuu@gmail.com>
2014-11-03 00:04:52 +0900

    add analyze feature

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package miniprofiler
 import (
 	"fmt"
 	"io"
+	"math"
+	"sort"
 	"strings"
 	"time"
 )
@@ -72,6 +74,165 @@ func Flush(writer io.Writer) {
 		fmt.Fprintln(writer, strings.Join(outputs, "\t"))
 	}
 	mp.profiles = make([]*MiniProfilerData, 0)
+}
+
+type Measure struct {
+	Description string
+	Count       int
+	Total       int
+	Mean        int
+	Min         int
+	Max         int
+}
+
+type By func(a, b *Measure) bool
+
+func (by By) Sort(measures []*Measure) {
+	ms := &measureSorter{
+		measures: measures,
+		by:       by,
+	}
+	sort.Sort(ms)
+}
+
+type measureSorter struct {
+	measures []*Measure
+	by       func(a, b *Measure) bool
+}
+
+func (s *measureSorter) Len() int {
+	return len(s.measures)
+}
+
+func (s *measureSorter) Swap(i, j int) {
+	s.measures[i], s.measures[j] = s.measures[j], s.measures[i]
+}
+
+func (s *measureSorter) Less(i, j int) bool {
+	return s.by(s.measures[i], s.measures[j])
+}
+
+type Column struct {
+	Name    string
+	Summary string
+	Sort    By
+}
+
+var (
+	columns = []*Column{
+		&Column{Name: "Count", Summary: "Count", Sort: func(a, b *Measure) bool { return a.Count > b.Count }},
+		&Column{Name: "Total", Summary: "Total", Sort: func(a, b *Measure) bool { return a.Total > b.Total }},
+		&Column{Name: "Mean", Summary: "Mean", Sort: func(a, b *Measure) bool { return a.Mean > b.Mean }},
+		&Column{Name: "Max", Summary: "Maximum(100 Percentile)", Sort: func(a, b *Measure) bool { return a.Max > b.Max }},
+	}
+)
+
+type ByTime []*Time
+
+type Time struct {
+	Url  string
+	Time int64
+}
+
+func (a ByTime) Len() int           { return len(a) }
+func (a ByTime) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByTime) Less(i, j int) bool { return a[i].Time > a[j].Time }
+
+func getIntegerDigitWidth(i int) int {
+	var w int
+	switch {
+	case i < 1:
+		w = 1
+	default:
+		w = int(math.Log10(float64(i)) + 1)
+	}
+	return w
+}
+
+func showMeasures(writer io.Writer, measures []*Measure) {
+	countWidth := 5
+	totalWidth := 5
+	meanWidth := 5
+	maxWidth := 5
+
+	for _, m := range measures {
+		var w int
+		w = getIntegerDigitWidth(m.Count)
+		if countWidth < w {
+			countWidth = w
+		}
+		w = getIntegerDigitWidth(m.Total)
+		if totalWidth < w {
+			totalWidth = w
+		}
+		w = getIntegerDigitWidth(m.Mean)
+		if meanWidth < w {
+			meanWidth = w
+		}
+		w = getIntegerDigitWidth(m.Max)
+		if maxWidth < w {
+			maxWidth = w
+		}
+	}
+
+	var format string
+	for _, column := range columns {
+		switch column.Name {
+		case "Count":
+			fmt.Fprintf(writer, fmt.Sprintf("%%%ds  ", countWidth), column.Name)
+			format += fmt.Sprintf("%%%dd  ", countWidth)
+		case "Total":
+			fmt.Fprintf(writer, fmt.Sprintf("%%%ds  ", totalWidth), column.Name)
+			format += fmt.Sprintf("%%%dd  ", totalWidth)
+		case "Mean":
+			fmt.Fprintf(writer, fmt.Sprintf("%%%ds  ", meanWidth), column.Name)
+			format += fmt.Sprintf("%%%dd  ", meanWidth)
+		default:
+			fmt.Fprintf(writer, fmt.Sprintf("%%%ds  ", maxWidth), column.Name)
+			format += fmt.Sprintf("%%%dd  ", maxWidth)
+		}
+	}
+	fmt.Fprintf(writer, "Description\n")
+	format += "%s\n"
+
+	for _, m := range measures {
+		fmt.Fprintf(writer, format, m.Count, m.Total, m.Mean, m.Min, m.Max, m.Description)
+	}
+}
+
+func Analyze(writer io.Writer) {
+	var times = make(map[string][]int)
+	var totals = make(map[string]int)
+	for _, prof := range mp.profiles {
+		for tag, val := range prof.steps {
+			times[tag] = append(times[tag], int(val))
+			totals[tag] += int(val)
+		}
+	}
+	var measures []*Measure
+	for tag, times := range times {
+		sort.Ints(times)
+		count := len(times)
+		total := totals[tag]
+		m := &Measure{
+			Description: tag,
+			Count:       count,
+			Total:       total,
+			Mean:        total / count,
+			Min:         times[0],
+			Max:         times[count-1],
+		}
+		measures = append(measures, m)
+	}
+
+	for _, column := range columns {
+		if column.Sort != nil {
+			fmt.Fprintf(writer, "Sort by %s\n", column.Summary)
+			By(column.Sort).Sort(measures)
+			showMeasures(writer, measures)
+			fmt.Fprintln(writer)
+		}
+	}
 }
 
 func (mpd *MiniProfilerData) Step(tag string) {
