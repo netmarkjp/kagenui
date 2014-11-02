f43203831baaa84fd4f08e59d9e0f4cf5a2d931e
Toshiaki Baba <toshiaki@netmark.jp>
2014-11-02 11:27:05 +0900

    initial import

--- README.md
+++ README.md
@@ -2,3 +2,66 @@ miniprofiler
 ============
 
 miniprofiler for golang
+
+Good for use with web application.
+
+Usage
+============
+
+```
+go get github.com/netmarkjp/miniprofiler
+```
+
+
+```
+import "github.com/netmarkjp/miniprofiler"
+
+func SomeHandler(){
+    mp := miniprofiler.Begin("someHandler")
+    defer mp.End()
+
+    ...
+
+    mp.Step("exec XXX")
+
+    ...
+
+    mp.Step("exec YYY")
+
+}
+
+func ProfileHandler(){
+    miniprofiler.Flush()
+}
+```
+
+Outout
+============
+
+ltsv format
+
+```
+log:MP<TAB><DESCRIPTION_AT_STEP>:<SPENT_TIME_IN_NANOSEC><TAB>...<TAB>description:<DESCRIPTION_AT_BEGIN>
+```
+
+Customize
+============
+
+## Enable/Disable
+
+```
+miniprofiler.Enable()
+```
+
+```
+miniprofiler.Disable()
+```
+
+## Change Writer
+
+default: ``os.Stdout``
+
+```
+miniprofiler.SetWriter(writer)
+```
+
---
+++ main.go
@@ -0,0 +1,81 @@
+package miniprofiler
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+var (
+	mp      *MiniProfiler
+	writer  io.Writer
+	enabled bool
+)
+
+type MiniProfilerData struct {
+	description string
+	steps       map[string]int64
+	lastStep    time.Time
+}
+
+type MiniProfiler struct {
+	profiles []*MiniProfilerData
+}
+
+func init() {
+	writer = os.Stdout
+	mp = new(MiniProfiler)
+	mp.profiles = make([]*MiniProfilerData, 0)
+	enabled = true
+}
+
+func SetWriter(w io.Writer) {
+	writer = w
+}
+
+func Enable() {
+	enabled = true
+}
+
+func Disable() {
+	enabled = false
+}
+
+func Begin(description string) *MiniProfilerData {
+	return &MiniProfilerData{description, make(map[string]int64, 0), time.Now()}
+}
+
+func Flush() {
+	for _, prof := range mp.profiles {
+		out := "log:MP"
+		for tag, val := range prof.steps {
+			out = fmt.Sprintf("%s\t%s:%d", out, tag, val)
+		}
+		out = fmt.Sprintf("%s\tdescription:%s", out, prof.description)
+		fmt.Fprintln(writer, out)
+	}
+	mp.profiles = make([]*MiniProfilerData, 0)
+}
+
+func (mpd *MiniProfilerData) Step(tag string) {
+	if !enabled {
+		return
+	}
+	now := time.Now()
+	thisstep := now.Sub(mpd.lastStep).Nanoseconds()
+
+	mpd.steps[tag] = thisstep
+	mpd.lastStep = now
+}
+
+func (mpd *MiniProfilerData) End() {
+	if !enabled {
+		return
+	}
+	now := time.Now()
+	thisstep := now.Sub(mpd.lastStep).Nanoseconds()
+
+	mpd.steps["Last Step to End"] = thisstep
+	mp.profiles = append(mp.profiles, mpd)
+}
